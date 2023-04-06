5c7f161f988b9191c79f41cce6f74e979ae6a0c8
ArunimaShukla03 <arunimashukla2003@gmail.com>
2023-04-06 14:47:01 +0530

    configuring django signals

--- signals/posts/apps.py
+++ signals/posts/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
 
+# We also need to configure the signals (when we make another file called "signals.py")
 
 class PostsConfig(AppConfig):
+
     default_auto_field = 'django.db.models.BigAutoField'
+
     name = 'posts'
+
+    '''
+    (configuring the signals)
+
+    def ready(self):
+        import base.signals
+    
+    '''
+
+'''this code will only run if we add "appname.apps.AppnameConfig" in installed apps in settings.py'''
